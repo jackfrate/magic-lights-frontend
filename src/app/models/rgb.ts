f23ce84b90ec2b53a65ed57e3fab92da8db2f41b
Jack Frate <yeet>
2021-08-21 22:40:41 -0500

    saving

---
+++ src/app/models/rgb.ts
@@ -0,0 +1,17 @@
+export interface RGB {
+    r: number;
+    g: number;
+    b: number;
+}
+// can have static functions to convert light to other siht and vice versa
+
+export interface Brightness {
+    bright: number;
+}
+
+export interface LightStatus {
+    r: number;
+    g: number;
+    b: number;
+    bright: number;
+}
---
+++ src/stuff.ts
@@ -0,0 +1,7 @@
+// base ip for rest calls 
+export const BASE_IP = '192.168.1.222:5000'
+
+// used for security
+export interface ListRes<T> {
+    list: T[]
+}
